72e03690f892f4269e1376bd388172c7c0c9e870
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2023-11-29 16:48:27 -0800

    adding code to git

---
+++ ManyStagesServer/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT ||5001;
+
+app.get('/', (req, res) => {
+    res.send(`Hello World from the backend server on ${port}!`)
+});
+app.listen(port, () => console.log(`Backend server running on port ${port}`));
